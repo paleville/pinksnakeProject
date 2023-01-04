ef2b4613cb0275f7984ace461aea21c4cd76ccb5
Christian <50929416+paleville@users.noreply.github.com>
2023-01-04 14:03:21 +0100

    Seccion 3
    Esta es la seccion 3

--- pinksnake/Game.cpp
+++ pinksnake/Game.cpp
@@ -3,7 +3,7 @@
 Game::Game()
 {
 	this->initWindow(); //inicializa la ventana
-	//this->initStates();
+	this->initStates();
 }
 
 Game::~Game()
@@ -83,69 +83,53 @@ void Game::initWindow()
 
 void Game::initStates()
 {
+
+	int mapWidth = 10; //ancho del mapa	
+	int mapHeight = 10; //altura del mapa
+
+	sf::Texture tileset_texture; //textura
+	sf::Sprite tileset_sprite; //sprite
+
+	tileset_texture.loadFromFile("tileset3.png"); //se carga el archivo
+	tileset_sprite.setTexture(tileset_texture); //se convierte el archivo a sprite
+
+
+	int map[] =
+	{
+		0,1,1,1,1,2,1,1,1,2,
+		1,1,1,1,1,1,1,1,1,1,
+		1,1,1,1,3,1,1,1,1,1,
+		1,1,1,1,1,1,1,1,1,1,
+		2,1,1,1,1,2,1,1,1,2,
+		1,1,1,1,1,1,1,1,1,1,
+		1,1,1,1,1,3,1,1,1,1,
+		2,1,1,1,2,1,1,1,1,1,
+		1,1,1,1,1,1,1,1,1,1,
+		3,1,1,1,1,0,1,1,1,0,
+
+	};
+
+	for (int y = 0; y < mapHeight; y++) //for loop de la altura del mapa
+	{
+		for (int x = 0; x < mapWidth; x++) //for loop de la anchura del mapa
+		{
+			int tilecount = x + y * 10;
+
+			tileset_sprite.setPosition(x * 16.f, y * 16.f); //calcular las dimensiones de cada cuadrado de la textura/sprite
+			tileset_sprite.setTextureRect(sf::IntRect(map[tilecount] * 16, 0, 16, 16));
+
+			this->window->draw(tileset_sprite);
+		}
+	}
+
+	this->states.push(new GameState(this->window)); //STAR se inicializa el GameState
+
+
 	/// <summary>
 	/// /
 	/// </summary>
 
-	sf::Texture tileTexture;
-	sf::Sprite tiles;
-
-
-	std::vector<std::vector<sf::Vector2i>> map; //vector de SFML map
-	std::vector<sf::Vector2i> tempMap; //mapa temporal
-
-	std::ifstream openMap1("map1.txt"); // //esta carga el archivo de texto
 	
-
-
-
-	if (openMap1.is_open()) //si el archivo esta abierto
-	{
-		std::string tileLocation;
-		openMap1 >> tileLocation;
-
-		tileTexture.loadFromFile(tileLocation); //carga el archivo de la textura
-		tiles.setTexture(tileTexture); //la textura se convierte en sprite
-
-		while (!openMap1.eof()) //mientras no se lea hasta el final del archivo
-		{
-			this->states.push(new MainMenuState(this->window)); //STAR se inicializa el GameState
-
-			std::string str;
-			openMap1 >> str;
-
-			char x = str[0], y = str[2];
-
-			if (!isdigit(x) || !isdigit(y))
-				tempMap.push_back(sf::Vector2i(-1, -1));
-			else
-				tempMap.push_back(sf::Vector2i(x - '0', y - '0'));
-
-			if (openMap1.peek() == '\n')
-			{
-				map.push_back(tempMap);
-				tempMap.clear();
-			}
-		}
-		map.push_back(tempMap);
-	}
-
-
-	
-
-
-	for (int i = 0; i < map.size(); i++)
-	{
-		for (int j = 0; j < map[i].size(); j++)
-		{
-			if (map[i][j].x != -1 && map[i][j].y != -1)
-			{
-				tiles.setPosition(i * 32, j * 32);
-				tiles.setTextureRect(sf::IntRect(map[i][j].x * 32, map[i][j].y * 32, 32, 32));
-				this->window->draw(tiles); //dibuja los tiles
-			}
-		}
-	}
 
 	/// <summary>
 	/// //
